59cfd6301be2a79df287a478fa64fc7927c7e05a
Elena Faillace <ele.faillace@gmail.com>
2025-04-09 13:44:23 +0100

    added animal and fov to names of data

--- tools/alignment.py
+++ tools/alignment.py
@@ -5,94 +5,108 @@ from scipy.linalg import qr, svd, inv
 import logging
 
 
-# Function CCA from Mo's
-def canoncorr(X:np.array, Y: np.array, fullReturn: bool = False) -> np.array:
+# Function CCA from Mo's
+def canoncorr(X: np.array, Y: np.array, fullReturn: bool = False) -> np.array:
     """
     Canonical Correlation Analysis (CCA)
     line-by-line port from Matlab implementation of `canoncorr`
     X,Y: (samples/observations) x (features) matrix, for both: X.shape[0] >> X.shape[1]
     fullReturn: whether all outputs should be returned or just `r` be returned (not in Matlab)
-    
-    returns: A,B,r,U,V 
+
+    returns: A,B,r,U,V
     A,B: Canonical coefficients for X and Y
     U,V: Canonical scores for the variables X and Y
     r:   Canonical correlations
-    
+
     Signature:
     A,B,r,U,V = canoncorr(X, Y)
     """
     n, p1 = X.shape
     p2 = Y.shape[1]
     if p1 >= n or p2 >= n:
-        logging.warning('Not enough samples, might cause problems')
+        logging.warning("Not enough samples, might cause problems")
 
     # Center the variables
-    X = X - np.mean(X,0)
-    Y = Y - np.mean(Y,0)
+    X = X - np.mean(X, 0)
+    Y = Y - np.mean(Y, 0)
 
     # Factor the inputs, and find a full rank set of columns if necessary
-    Q1,T11,perm1 = qr(X, mode='economic', pivoting=True, check_finite=True)
+    Q1, T11, perm1 = qr(X, mode="economic", pivoting=True, check_finite=True)
 
-    rankX = sum(np.abs(np.diagonal(T11)) > np.finfo(type((np.abs(T11[0,0])))).eps*max([n,p1]))
+    rankX = sum(
+        np.abs(np.diagonal(T11))
+        > np.finfo(type((np.abs(T11[0, 0])))).eps * max([n, p1])
+    )
 
     if rankX == 0:
-        logging.error(f'stats:canoncorr:BadData = X')
+        logging.error(f"stats:canoncorr:BadData = X")
     elif rankX < p1:
-        logging.warning('stats:canoncorr:NotFullRank = X')
-        Q1 = Q1[:,:rankX]
-        T11 = T11[:rankX,:rankX]
+        logging.warning("stats:canoncorr:NotFullRank = X")
+        Q1 = Q1[:, :rankX]
+        T11 = T11[:rankX, :rankX]
 
-    Q2,T22,perm2 = qr(Y, mode='economic', pivoting=True, check_finite=True)
-    rankY = sum(np.abs(np.diagonal(T22)) > np.finfo(type((np.abs(T22[0,0])))).eps*max([n,p2]))
+    Q2, T22, perm2 = qr(Y, mode="economic", pivoting=True, check_finite=True)
+    rankY = sum(
+        np.abs(np.diagonal(T22))
+        > np.finfo(type((np.abs(T22[0, 0])))).eps * max([n, p2])
+    )
 
     if rankY == 0:
-        logging.error(f'stats:canoncorr:BadData = Y')
+        logging.error(f"stats:canoncorr:BadData = Y")
     elif rankY < p2:
-        logging.warning('stats:canoncorr:NotFullRank = Y')
-        Q2 = Q2[:,:rankY]
-        T22 = T22[:rankY,:rankY]
+        logging.warning("stats:canoncorr:NotFullRank = Y")
+        Q2 = Q2[:, :rankY]
+        T22 = T22[:rankY, :rankY]
 
     # Compute canonical coefficients and canonical correlations.  For rankX >
     # rankY, the economy-size version ignores the extra columns in L and rows
     # in D. For rankX < rankY, need to ignore extra columns in M and D
     # explicitly. Normalize A and B to give U and V unit variance.
-    d = min(rankX,rankY)
-    L,D,M = svd(Q1.T @ Q2, full_matrices=True, check_finite=True, lapack_driver='gesdd')
+    d = min(rankX, rankY)
+    L, D, M = svd(
+        Q1.T @ Q2, full_matrices=True, check_finite=True, lapack_driver="gesdd"
+    )
     M = M.T
 
-    if np.isnan(T11).any() or np.isnan(T22).any() or np.isinf(T11).any() or np.isinf(T22).any():
-        logging.error('stats:canoncorr:BadData = X or Y')
-    A = inv(T11) @ L[:,:d] * np.sqrt(n-1)
-    B = inv(T22) @ M[:,:d] * np.sqrt(n-1)
+    if (
+        np.isnan(T11).any()
+        or np.isnan(T22).any()
+        or np.isinf(T11).any()
+        or np.isinf(T22).any()
+    ):
+        logging.error("stats:canoncorr:BadData = X or Y")
+    A = inv(T11) @ L[:, :d] * np.sqrt(n - 1)
+    B = inv(T22) @ M[:, :d] * np.sqrt(n - 1)
     r = D[:d]
     # remove roundoff errs
-    r[r>=1] = 1
-    r[r<=0] = 0
+    r[r >= 1] = 1
+    r[r <= 0] = 0
 
     if not fullReturn:
         return r
 
     ### FROME HERE ###
     # Put coefficients back to their full size and their correct order
-    # Adaptation from MATLAB: Assign to A the correct size by taking the first elements of A and putting them in the order of perm1
-    stackedA = np.vstack((A, np.zeros((p1-rankX,d))))
+    # Adaptation from MATLAB: Assign to A the correct size by taking the first elements of A and putting them in the order of perm1
+    stackedA = np.vstack((A, np.zeros((p1 - rankX, d))))
     newA = np.zeros(stackedA.shape)
     for stackedA_idx, newA_idx in enumerate(perm1):
-        newA[newA_idx,:] = stackedA[stackedA_idx,:]
-    
-    stackedB = np.vstack((B, np.zeros((p2-rankY,d))))
+        newA[newA_idx, :] = stackedA[stackedA_idx, :]
+
+    stackedB = np.vstack((B, np.zeros((p2 - rankY, d))))
     newB = np.zeros(stackedB.shape)
     for stackedB_idx, newB_idx in enumerate(perm2):
-        newB[newB_idx,:] = stackedB[stackedB_idx,:]
+        newB[newB_idx, :] = stackedB[stackedB_idx, :]
     ### TO HERE ###
-    
+
     # Compute the canonical variates
     U = X @ newA
     V = Y @ newB
 
     return newA, newB, r, U, V
 
-def procrustes(X, Y, scaling=True, reflection='best'):
+
+def procrustes(X, Y, scaling=True, reflection="best"):
     """
     A port of MATLAB's `procrustes` function to Numpy.
 
@@ -105,12 +119,12 @@ def procrustes(X, Y, scaling=True, reflection='best'):
 
     Inputs:
     ------------
-    X, Y    
+    X, Y
         matrices of target and input coordinates. they must have equal
         numbers of  points (rows), but Y may have fewer dimensions
         (columns) than X.
 
-    scaling 
+    scaling
         if False, the scaling component of the transformation is forced
         to 1
 
@@ -122,21 +136,21 @@ def procrustes(X, Y, scaling=True, reflection='best'):
 
     Outputs
     ------------
-    d       
+    d
         the residual sum of squared errors, normalized according to a
         measure of the scale of X, ((X - X.mean(0))**2).sum()
 
     Z
         the matrix of transformed Y-values
 
-    tform   
+    tform
         a dict specifying the rotation, translation and scaling that
         maps X --> Y
 
     """
 
-    n,m = X.shape
-    ny,my = Y.shape
+    n, m = X.shape
+    ny, my = Y.shape
 
     muX = X.mean(0)
     muY = Y.mean(0)
@@ -144,8 +158,8 @@ def procrustes(X, Y, scaling=True, reflection='best'):
     X0 = X - muX
     Y0 = Y - muY
 
-    ssX = (X0**2.).sum()
-    ssY = (Y0**2.).sum()
+    ssX = (X0**2.0).sum()
+    ssY = (Y0**2.0).sum()
 
     # centred Frobenius norm
     normX = np.sqrt(ssX)
@@ -156,29 +170,27 @@ def procrustes(X, Y, scaling=True, reflection='best'):
     Y0 /= normY
 
     if my < m:
-        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
+        Y0 = np.concatenate((Y0, np.zeros(n, m - my)), 0)
 
     # optimum rotation matrix of Y
     A = np.dot(X0.T, Y0)
-    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    U, s, Vt = np.linalg.svd(A, full_matrices=False)
     V = Vt.T
     T = np.dot(V, U.T)
 
-    if reflection != 'best':
-
+    if reflection != "best":
         # does the current solution use a reflection?
         have_reflection = np.linalg.det(T) < 0
 
         # if that's not what was specified, force another reflection
         if reflection != have_reflection:
-            V[:,-1] *= -1
+            V[:, -1] *= -1
             s[-1] *= -1
             T = np.dot(V, U.T)
 
     traceTA = s.sum()
 
     if scaling:
-
         # optimum scaling of Y
         b = traceTA * normX / normY
 
@@ -186,19 +198,19 @@ def procrustes(X, Y, scaling=True, reflection='best'):
         d = 1 - traceTA**2
 
         # transformed coords
-        Z = normX*traceTA*np.dot(Y0, T) + muX
+        Z = normX * traceTA * np.dot(Y0, T) + muX
 
     else:
         b = 1
-        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
-        Z = normY*np.dot(Y0, T) + muX
+        d = 1 + ssY / ssX - 2 * traceTA * normY / normX
+        Z = normY * np.dot(Y0, T) + muX
 
     # transformation matrix
     if my < m:
-        T = T[:my,:]
-    c = muX - b*np.dot(muY, T)
-    
-    #transformation values 
-    tform = {'rotation':T, 'scale':b, 'translation':c}
-   
+        T = T[:my, :]
+    c = muX - b * np.dot(muY, T)
+
+    # transformation values
+    tform = {"rotation": T, "scale": b, "translation": c}
+
     return d, Z, tform
